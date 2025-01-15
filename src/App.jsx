95d2acddccec7b693266bd659be883f2faaad15e
FrancescoTullio <francescotullio1992@gmail.com>
2025-01-15 12:54:38 +0100

    feat:milestone 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,103 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
-import CardComponet from "./component/CardComponent";
-import SerchComponent from "./component/SerchComponent";
+import CardComponent from "./component/CardComponent";
 
 
 function App() {
 
-  const urlApi = "https://api.themoviedb.org/3/search/movie";
+  const urlApiFilms = "https://api.themoviedb.org/3/search/movie";
+  const urlApiSeries = "https://api.themoviedb.org/3/search/tv"
 
   const [imputValue, setImputValue] = useState("")
-  const [arrayFilms, setArrayFilms] = useState([]);
+  const [arrayFilms, setArrayFilms] = useState([])
+  const [arraySeries, setArraySeries] = useState([])
 
   const getMovie = () => {
-    axios(urlApi, {params: {
-      api_key: "e5ff284d647bae3c9c2b12b677e9e728",
-      query: imputValue
-    }}
+    axios(urlApiFilms, {
+      params: {
+        api_key: "e5ff284d647bae3c9c2b12b677e9e728",
+        query: imputValue
+      }
+    }
     ).then((resp) => {
+      console.log(resp.data.results);
+      
       const newArray = resp.data.results;
       setArrayFilms(newArray)
     })
   }
 
-  const resetAll = () => {
-    setArrayFilms([]);
-    setImputValue("")
+  const getSeries = () => {
+    axios(urlApiSeries, {
+      params: {
+        api_key: "e5ff284d647bae3c9c2b12b677e9e728",
+        query: imputValue
+      }
+    }).then((resp) => {
+      const newArray = resp.data.results;
+      setArraySeries(newArray);
+    })
   }
 
- 
+  const getAll = () => {
+    getSeries();
+    getMovie();
+  }
+
+  const resetAll = () => {
+    setArrayFilms([]);
+    setArraySeries([]);
+    setImputValue("");
+  }
+
+
 
   return (
     <>
-      <section>
-        <input type="text" value={imputValue} onChange={(event) => { setImputValue(event.target.value) }} />
-        <button onClick={() => getMovie()}>cerca</button>
-        <button onClick={() => resetAll()}>rest</button>
-      </section>
-      <section>
-        {
-          arrayFilms.map((curFilm, index) => {
-            return(
-              <div key={index}>
-                <CardComponet element={curFilm} />
-              </div>
-            )
-          })
-        }
-      </section>
+      <header className="row justy-content-conter al-items">
+        <section>
+          <input type="text" value={imputValue} onChange={(event) => { setImputValue(event.target.value) }} />
+          <button onClick={() => getAll()} className="ml-15">cerca</button>
+          <button onClick={() => resetAll()} className="ml-5">rest</button>
+        </section>
+      </header>
+      <main>
+        <section>
+          <div className="container">
+            {
+              arrayFilms.length !== 0  && <h2>-----films-----</h2>
+            }
+            <div className="row">
+              {
+                arrayFilms.map((curFilm, index) => {
+                  return (
+                    <div key={index} className="col-3">
+                      <CardComponent element={curFilm} />
+                    </div>
+                  )
+                })
+              }
+            </div>
+            {
+              arraySeries.length !== 0 && <h2>-----serie----</h2>
+            }
+            <div className="row">
+              {
+              arraySeries.map((curSerie, index) => {
+                return (
+                  <div key={index} className="col-3">
+                    <CardComponent element={curSerie} />
+                  </div>
+                )
+              })
+            }
+            </div>
+            
+
+
+          </div>
+        </section>
+      </main>
     </>
   )
 }
--- src/component/CardComponent.jsx
+++ src/component/CardComponent.jsx
@@ -1,16 +1,27 @@
 const CardComponent = ({element}) => {
-    console.log(element)
+    
     const { original_title, poster_path, title, vote_count, original_language } = element;
+    console.log(poster_path)
     return (
-        <div>
-            <div>
-                <img src={`https://image.tmdb.org/t/p/w342${poster_path}`} alt="{title}" />
+        <div className="card">
+            <div className="card-img">
+                {poster_path === null ? <img src="/img/placeholder-img.webp" alt="{title}" /> : <img src={`https://image.tmdb.org/t/p/w342${poster_path}`} alt="{title}" />} 
+                
             </div>
-            <div>
+            <div className="card-body">
                 <h3>{title}</h3>
                 <h4>{original_title}</h4>
-                <span>{original_language}</span>
-                <span>{vote_count}</span>
+                <p>{vote_count}</p>
+                {
+                    original_language === "it" ? <img src="/img/it.png" alt="{original_language}" /> : ""
+                }
+                 {
+                    original_language === "en" ? <img src="/img/en.png" alt="{original_language}" /> : ""
+                }
+                {
+                    original_language !== "en" && original_language !== "it" ? <img src="/img/bandiera.webp" alt="{original_language}" /> : ""
+                }
+                <img src="" alt="" />
             </div>
         </div>
     )
